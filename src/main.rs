391839168fbb8da2f87fe8e3333c7a0598b4da9d
zwpdbh <hyperion_z@outlook.com>
2023-10-07 23:37:11 +0800

    > chapter02

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ mod models {
 use models::question::{Question, QuestionId};
 use std::str::FromStr;
 
-fn main() {
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
     let question = Question::new(
         // QuestionId("1".to_string()),
         QuestionId::from_str("1").expect("No id provided"),
@@ -16,4 +19,7 @@ fn main() {
         Some(vec!["faq".to_string()]),
     );
     println!("{:#?}", question);
+
+    let hello = warp::get().map(|| format!("Hello, World!"));
+    warp::serve(hello).run(([127, 0, 0, 1], 8000)).await;
 }
